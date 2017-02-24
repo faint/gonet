41850483c28f959d3127f9dbfd40ab18a3a5397a
仲金德 <bunnyfaint@me.com>
2017-02-24 18:01:43 +0800

    add client

--- client.go
+++ client.go
@@ -1,1 +1,51 @@
 package server
+
+import (
+	"net"
+	"time"
+)
+
+const (
+	retryDelay = 1 // time.Second, to retry to dial client.
+)
+
+// Client save the other server as client.
+type Client struct {
+	address string // ip +":"+ port
+	status  int
+	Conn    net.Conn
+}
+
+// GetClient return client pointer and set the address.
+// but did not dial to target.
+func GetClient(ip, port string) *Client {
+	c := new(Client)
+
+	if ip != "" {
+		c.address = ip + ":" + port
+	}
+
+	c.status = Lost
+
+	open := false
+	for !open { // when can't connect, retry every 1 Second
+		err := c.Dial()
+		if err == nil {
+			open = true
+		}
+		time.Sleep(retryDelay * time.Second)
+	}
+
+	return c
+}
+
+// Dial dial target address.
+func (c *Client) Dial() error {
+	conn, err := net.Dial("tcp", c.address)
+	if err != nil {
+		return err
+	}
+	c.Conn = conn
+	c.status = Open
+	return nil
+}
